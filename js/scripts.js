e5c07acde80ce1a7f054bfe4ecfab9cf44fc787c
ahmadfathy <ahmadfathy77@gmail.com>
2021-07-10 03:29:42 +0200

    ready

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $(".level-three .card").append('<a href="javascript:void(0)" data-toggle="modal" data-target="#exampleModal" class="absolute_link"></a>');
+
+  //Horizental Scroll
+  $('.horiz_container_outer').horizontalScroll();
+});
